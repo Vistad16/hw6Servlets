e97d87bc3fa2368355ccbeb0a961dfb13c9e3937
Bohdan <88307368+Vistad16@users.noreply.github.com>
2022-08-06 11:48:24 +0300

    Delete src/main/java/com/goit/java5/demo directory

--- src/main/java/com/goit/java5/demo/forum/MessageStorage.java
+++
@@ -1,9 +0,0 @@
-package com.goit.java5.demo.forum;
-
-import java.util.List;
-
-public interface MessageStorage {
-	List<Message> getAllMessages();
-	void add(Message message);
-	void deleteById(String id);
-}
