146eb86f1bb7019883e480c66573874b40f1435c
Bohdan Datsiv <bohdan.datsiv@gmail.com>
2022-08-07 16:05:48 +0300

    MySQL

--- src/main/java/com/goit/java5/App.java
+++ src/main/java/com/goit/java5/App.java
@@ -1,24 +1,18 @@
 package com.goit.java5;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import com.goit.java5.prefs.Prefs;
-import com.goit.java5.storage.DatabaseInitService;
-import com.goit.java5.storage.Storage;
+import com.goit.java5.data.DatabaseInitService;
+import com.goit.java5.data.Storage;
 
 public class App {
 	public static void main(String[] args) {
-		Storage storage = Storage.getInstance();
+//		Storage storage = Storage.getInstance();
 
 		//test
 		//Connection connection = storage.getConnection();
 
 //		storage.executeUpdate("CREATE TABLE test_table (name VARCHAR(100))");
 
-		new DatabaseInitService().initDb(storage);
+//		new DatabaseInitService().initDb(storage);
 
 }
 }
