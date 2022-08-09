98bfe783707c021387d01907201c268de8d122a7
Bohdan Datsiv <bohdan.datsiv@gmail.com>
2022-08-09 17:23:08 +0300

    add customers part, done 5/5

--- src/main/java/com/goit/java5/servlet/command/CommandService.java
+++ src/main/java/com/goit/java5/servlet/command/CommandService.java
@@ -12,6 +12,10 @@ import com.goit.java5.servlet.command.companies.AddCommands;
 import com.goit.java5.servlet.command.companies.DeleteCommands;
 import com.goit.java5.servlet.command.companies.GetCommands;
 import com.goit.java5.servlet.command.companies.UpdateCommand;
+import com.goit.java5.servlet.command.customers.AddCustomersCommand;
+import com.goit.java5.servlet.command.customers.DeleteCustomersCommand;
+import com.goit.java5.servlet.command.customers.GetCustomersCommand;
+import com.goit.java5.servlet.command.customers.UpdateCustomersCommand;
 import com.goit.java5.servlet.command.developers.AddDevelopersCommand;
 import com.goit.java5.servlet.command.developers.DeleteDevelopersCommand;
 import com.goit.java5.servlet.command.developers.GetDevelopersCommand;
@@ -32,25 +36,30 @@ public class CommandService {
 		commands = new HashMap<>();
 		//todo add commands
 		//company
-		commands.put("GET /hw6Servlets/companies", new GetCommands());
-		commands.put("POST /hw6Servlets/companies/delete", new DeleteCommands());
 		commands.put("POST /hw6Servlets/companies", new AddCommands());
+		commands.put("POST /hw6Servlets/companies/delete", new DeleteCommands());
+		commands.put("GET /hw6Servlets/companies", new GetCommands());
 		commands.put("POST /hw6Servlets/companies/update", new UpdateCommand());
+		//customers
+		commands.put("POST /hw6Servlets/customers/add", new AddCustomersCommand());
+		commands.put("POST /hw6Servlets/customers/delete", new DeleteCustomersCommand());
+		commands.put("GET /hw6Servlets/customers", new GetCustomersCommand());
+		commands.put("POST /hw6Servlets/customers/update", new UpdateCustomersCommand());
 		//developers
-		commands.put("GET /hw6Servlets/developers", new GetDevelopersCommand());
 		commands.put("POST /hw6Servlets/developers/add", new AddDevelopersCommand());
 		commands.put("POST /hw6Servlets/developers/delete", new DeleteDevelopersCommand());
+		commands.put("GET /hw6Servlets/developers", new GetDevelopersCommand());
 		commands.put("POST /hw6Servlets/developers/update", new UpdateDevelopersCommand());
 		//projects
-		commands.put("GET /hw6Servlets/projects", new GetProjectsCommand());
 		commands.put("POST /hw6Servlets/projects/add", new AddProjectsCommand());
-		commands.put("POST /hw6Servlets/projects/update", new UpdateProjectsCommand());
 		commands.put("POST /hw6Servlets/projects/delete", new DeleteProjectsCommand());
+		commands.put("GET /hw6Servlets/projects", new GetProjectsCommand());
+		commands.put("POST /hw6Servlets/projects/update", new UpdateProjectsCommand());
 		//skills
-		commands.put("GET /hw6Servlets/skills", new GetSkillsCommand());
 		commands.put("POST /hw6Servlets/skills/add", new AddSkillsCommand());
-		commands.put("POST /hw6Servlets/skills/update", new UpdateSkillsCommand());
 		commands.put("POST /hw6Servlets/skills/delete", new DeleteSkillsCommand());
+		commands.put("GET /hw6Servlets/skills", new GetSkillsCommand());
+		commands.put("POST /hw6Servlets/skills/update", new UpdateSkillsCommand());
 	}
 
 	public void process(HttpServletRequest req, HttpServletResponse resp) throws IOException {
